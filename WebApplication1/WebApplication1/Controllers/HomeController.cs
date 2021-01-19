09f701342dbd6b60c7aff72d684ca22beedab8de
aggsergio <72625669+aggsergio@users.noreply.github.com>
2021-01-18 21:16:45 -0600

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Array arreglo;
+            List<ClienteModel> clientes = new List<ClienteModel>();
+            var a = new ClienteModel();
+
+            clientes.Add(new ClienteModel
+            {
+                Clave_Cliente = 123,
+                Nombre_Cliente = "Sergio",
+                Direccion_Cliente = "Av Francisco I Madero",
+                Pedido_Cliente = "Cerveza Indio"
+            });
+
+            clientes.Add(new ClienteModel
+            {
+                Clave_Cliente = 432,
+                Nombre_Cliente = "Fernando",
+                Direccion_Cliente = "Av Corregidora",
+                Pedido_Cliente = "Cerveza XX"
+            });
+            clientes.Add(new ClienteModel
+            {
+                Clave_Cliente = 567,
+                Nombre_Cliente = "Gerardo",
+                Direccion_Cliente = "Av 30 de Febrero",
+                Pedido_Cliente = "Cerveza Modelo"
+            });
+
+            arreglo = clientes.ToArray();
+            ViewBag.ClienteModel = arreglo;
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/ClienteModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Models
+{
+	public class ClienteModel
+	{
+		public int Clave_Cliente { get; set; }
+		public string Nombre_Cliente { get; set; }
+		public string Direccion_Cliente { get; set; }
+		public string Pedido_Cliente { get; set; }
+
+	}
+}
